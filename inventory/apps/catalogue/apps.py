72365617036c67dd1cdcd9a01e27f132c86c8f0f
SAI PREETHAM <98286937+PHANTOMTONYT@users.noreply.github.com>
2025-10-26 21:03:39 +0530

    Delete inventory directory

--- inventory/apps/catalogue/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CatalogueConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.catalogue'
-    verbose_name = 'Item Catalogue'
--- inventory/apps/rbac/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RbacConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.rbac'
-    verbose_name = 'Role-Based Access Control'
